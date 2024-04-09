79c157168a0ce94cfd909d0959077eadc5579ffd
Charlie Lin <lincharlie00@gmail.com>
2024-04-09 01:00:38 -0400

    bug fix: new user notification showing again

--- backend/pkg/websocket/pool.go
+++ backend/pkg/websocket/pool.go
@@ -1,6 +1,9 @@
 package websocket
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type Pool struct {
 	Register chan *Client
@@ -18,22 +21,31 @@ func NewPool() *Pool {
 	}
 }
 
-func (pool *Pool) Start() {
-    for {
-        select {
-        case client := <-pool.Register:
-            pool.Clients[client] = true
-            client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
-        case client := <-pool.Unregister:
-            delete(pool.Clients, client)
-            client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
-        case message := <-pool.Broadcast:
-            for client := range pool.Clients {
-                if err := client.Conn.WriteJSON(message); err != nil {
-                    fmt.Println("Error writing JSON message:", err)
-                    delete(pool.Clients, client)
-                }
+func (pool *Pool) Start()  {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			for client := range pool.Clients {
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			}
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			for client := range pool.Clients {
+				log.Println(client)
+                client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
             }
-        }
-    }
+		case message := <-pool.Broadcast:
+			fmt.Println("Sending message to all clients in Pool")
+			for client := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		}
+		
+	}
 }
